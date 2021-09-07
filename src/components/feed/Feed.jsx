43decee8396929737ab3e88f10c6e82446bf86a0
morronepablo <morronepablo@gmail.com>
2021-09-07 18:33:31 -0300

    subiendo proyecto

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Post } from '../post/Post'
+import { Share } from '../share/Share'
+import { Posts } from '../../dummyData'
+import "./feed.css"
+
+export const Feed = () => {
+    return (
+        <div className="feed">
+            <div className="feedWrapper">
+                <Share />
+                {Posts.map(p=>(
+                    <Post key={p.id} post={p} />
+                ))}
+            </div>
+        </div>
+    )
+}
